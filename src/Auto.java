9daeb3adb808c0883c446e2f87b8b124b4594646
vina <vina@VIAWSE02LC0208.duoc.acad>
2024-08-28 14:34:36 -0400

    probando git

---
+++ src/Auto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Maxi
+ * @version 28/08/2024
+ */
+public class Auto {
+    
+    private String marca,modelo,color,tipoTransmision,tipoCombustible;
+    private int año,cantidadPuertas;
+    private double cilindrada;
+    
+    
+}
